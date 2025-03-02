35a17c130546d76d14955dfb2557fca364a4a988
aiswarya-03-cloud <aiswaryaajith04@gmail.com>
2025-03-02 15:02:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,31 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),  tailwindcss()],
+// })
+
+
+
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-})
+  build: {
+
+     chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor-react';
+            }
+            if (id.includes('lodash')) {
+              return 'vendor-lodash';
+            }
+            return 'vendor'; // Other node_modules files
+          }
+        }
+      }
+    }
+  }
+});
+
